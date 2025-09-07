b6ab72c71ade1448d74f4ab9af4ab19d4eec8db4
Ayush Patidar <ayushpatidar814@gmail.com>
2025-09-07 21:06:01 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,8 +5,7 @@ const Footer = () => {
         <div className='fixed bottom-0 bg-slate-800 text-white flex justify-between items-center px-23 h-8 w-full pt-0'>
             <div className="logo font-bold text-white text-2xl">
                 <span className='text-green-500'> &lt;</span>
-
-                <span>Pass</span><span className='text-green-500'>OP/&gt;</span>    
+                <span>DigiPass</span><span className='text-green-500'>&gt;</span>    
             </div>
             <div className='flex justify-center items-center'> Created with <img className='invert w-7 mx-2' src="/favourite-stroke-rounded.svg" alt="icon" /> by CodeWithAyush </div>
         </div>
