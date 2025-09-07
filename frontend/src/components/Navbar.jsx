e2de65e24ca481b7d564855e18566e1703032309
Ayush Patidar <ayushpatidar814@gmail.com>
2025-09-07 21:10:40 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className="mycontainer flex justify-between items-center px-4 py-3 h-12">
                 <div className="logo font-bold text-white text-xl">
                     <span className='text-green-500'> &lt;</span>
-                    <span>DigiPass</span><span className='text-green-500'>OP/&gt;</span>
+                    <span>DigiPass</span><span className='text-green-500'>&gt;</span>
                     </div>
                 <button className='text-white bg-green-700 my-3 mx-2 rounded-full flex justify-between items-center ring-white ring-1'> 
                     <img className='invert w-10 p-1' src="/github.svg" alt="github logo" />
